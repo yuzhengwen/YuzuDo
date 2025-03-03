9423d2664b5e022e9ec54df1631d5ae8e99cfb75
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-03-04 03:57:02 +0800

    Update deployment.py

--- yuzudo/deployment.py
+++ yuzudo/deployment.py
@@ -30,7 +30,7 @@ STORAGES = {
     },
 }
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(';')}
+CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
