a99fb13d6b3e9d98b9dba2a7ee9c7a0afc9ee20d
Yu Zhengwen <yuzhengwen0217@gmail.com>
2025-03-04 03:02:29 +0800

    Update settings.py
    Removed using frontend/build for deployment

--- yuzudo/settings.py
+++ yuzudo/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static')
+    # os.path.join(BASE_DIR, 'frontend/build/static')
+    # For development, we want to serve the static files from the frontend (React) build folder
 ]
 
 # Default primary key field type
